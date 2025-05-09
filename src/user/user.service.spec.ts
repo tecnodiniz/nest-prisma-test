911dc8c7feb7d4cee418eaac00c6866ca5a71131
Eduardo Diniz <diniz480@gmail.com>
2025-05-09 16:43:15 -0300

    feat: add createUser service and unit test

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -23,6 +23,13 @@ describe('UserService', () => {
 
   const prismaMock = {
     user: {
+      create: jest
+        .fn()
+        .mockImplementation(({ data: { id, name, email, password } }) => {
+          const newUser = { id, name, email, password };
+          mockUsers.push(newUser);
+          return Promise.resolve(newUser);
+        }),
       findMany: jest.fn().mockResolvedValue(mockUsers),
       findUnique: jest.fn().mockImplementation(({ where: { id } }) => {
         return Promise.resolve(mockUsers.find((user) => user.id === id));
@@ -59,5 +66,22 @@ describe('UserService', () => {
     const result = await service.fetchUser('1');
     expect(result).toEqual(mockUsers[0]);
     expect(prisma.user.findUnique).toHaveBeenCalledWith({ where: { id: '1' } });
+  });
+
+  it('should create a new user', async () => {
+    const newUser: Partial<User> = {
+      id: '3',
+      name: 'Henrique Eduardo',
+      email: 'diniz@yahoo.com',
+      password: 'password',
+    };
+
+    prismaMock.user.create.mockResolvedValue(newUser);
+
+    const result = await service.createUser(newUser);
+
+    expect(prisma.user.create).toHaveBeenCalledWith({ data: newUser });
+    expect(result).toEqual(newUser);
+    expect(mockUsers.length).toBe(2);
   });
 });
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
+  async createUser(data: Partial<User>) {
+    return this.prisma.user.create({ data });
+  }
   async fetchAllUsers() {
     return this.prisma.user.findMany();
   }
